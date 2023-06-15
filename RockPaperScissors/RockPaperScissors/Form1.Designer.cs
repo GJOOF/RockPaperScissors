74eede2a815f4ad5d4f53a1eb369846279181131
GJOOF <134306936+GJOOF@users.noreply.github.com>
2023-06-15 18:52:28 +0300

    Add files via upload

---
+++ RockPaperScissors/RockPaperScissors/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace RockPaperScissors
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.lab_score = new System.Windows.Forms.Label();
+            this.lab_time = new System.Windows.Forms.Label();
+            this.lab_round = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lab_choose_turn = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Pb_Scissors = new System.Windows.Forms.PictureBox();
+            this.Pb_Paper = new System.Windows.Forms.PictureBox();
+            this.Pb_Rock = new System.Windows.Forms.PictureBox();
+            this.Player_PictureBox = new System.Windows.Forms.PictureBox();
+            this.AI_PictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Pb_Scissors)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pb_Paper)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pb_Rock)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player_PictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AI_PictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lab_score
+            // 
+            this.lab_score.AutoSize = true;
+            this.lab_score.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.7F, System.Drawing.FontStyle.Bold);
+            this.lab_score.Location = new System.Drawing.Point(361, 74);
+            this.lab_score.Name = "lab_score";
+            this.lab_score.Size = new System.Drawing.Size(64, 29);
+            this.lab_score.TabIndex = 2;
+            this.lab_score.Text = "0 - 0";
+            this.lab_score.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lab_time
+            // 
+            this.lab_time.AutoSize = true;
+            this.lab_time.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lab_time.Location = new System.Drawing.Point(382, 136);
+            this.lab_time.Name = "lab_time";
+            this.lab_time.Size = new System.Drawing.Size(73, 29);
+            this.lab_time.TabIndex = 3;
+            this.lab_time.Text = "Time";
+            this.lab_time.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lab_round
+            // 
+            this.lab_round.AutoSize = true;
+            this.lab_round.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lab_round.Location = new System.Drawing.Point(334, 225);
+            this.lab_round.Name = "lab_round";
+            this.lab_round.Size = new System.Drawing.Size(102, 29);
+            this.lab_round.TabIndex = 4;
+            this.lab_round.Text = "Rounds";
+            this.lab_round.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(615, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "AI";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(124, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Player";
+            // 
+            // lab_choose_turn
+            // 
+            this.lab_choose_turn.AutoSize = true;
+            this.lab_choose_turn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lab_choose_turn.Location = new System.Drawing.Point(295, 310);
+            this.lab_choose_turn.Name = "lab_choose_turn";
+            this.lab_choose_turn.Size = new System.Drawing.Size(185, 25);
+            this.lab_choose_turn.TabIndex = 7;
+            this.lab_choose_turn.Text = "Choose your turn:";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // Pb_Scissors
+            // 
+            this.Pb_Scissors.BackColor = System.Drawing.Color.Transparent;
+            this.Pb_Scissors.Image = ((System.Drawing.Image)(resources.GetObject("Pb_Scissors.Image")));
+            this.Pb_Scissors.Location = new System.Drawing.Point(473, 349);
+            this.Pb_Scissors.Name = "Pb_Scissors";
+            this.Pb_Scissors.Size = new System.Drawing.Size(70, 70);
+            this.Pb_Scissors.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Pb_Scissors.TabIndex = 10;
+            this.Pb_Scissors.TabStop = false;
+            this.Pb_Scissors.Click += new System.EventHandler(this.Pb_Scissors_Click);
+            // 
+            // Pb_Paper
+            // 
+            this.Pb_Paper.BackColor = System.Drawing.Color.Transparent;
+            this.Pb_Paper.Image = ((System.Drawing.Image)(resources.GetObject("Pb_Paper.Image")));
+            this.Pb_Paper.Location = new System.Drawing.Point(366, 349);
+            this.Pb_Paper.Name = "Pb_Paper";
+            this.Pb_Paper.Size = new System.Drawing.Size(70, 70);
+            this.Pb_Paper.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Pb_Paper.TabIndex = 9;
+            this.Pb_Paper.TabStop = false;
+            this.Pb_Paper.Click += new System.EventHandler(this.Pb_Paper_Click);
+            // 
+            // Pb_Rock
+            // 
+            this.Pb_Rock.BackColor = System.Drawing.Color.Transparent;
+            this.Pb_Rock.Image = ((System.Drawing.Image)(resources.GetObject("Pb_Rock.Image")));
+            this.Pb_Rock.Location = new System.Drawing.Point(255, 349);
+            this.Pb_Rock.Name = "Pb_Rock";
+            this.Pb_Rock.Size = new System.Drawing.Size(70, 70);
+            this.Pb_Rock.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Pb_Rock.TabIndex = 8;
+            this.Pb_Rock.TabStop = false;
+            this.Pb_Rock.Click += new System.EventHandler(this.Pb_Rock_Click);
+            // 
+            // Player_PictureBox
+            // 
+            this.Player_PictureBox.BackColor = System.Drawing.Color.Transparent;
+            this.Player_PictureBox.Image = global::RockPaperScissors.Properties.Resources.question_mark;
+            this.Player_PictureBox.Location = new System.Drawing.Point(46, 74);
+            this.Player_PictureBox.Name = "Player_PictureBox";
+            this.Player_PictureBox.Size = new System.Drawing.Size(200, 200);
+            this.Player_PictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Player_PictureBox.TabIndex = 1;
+            this.Player_PictureBox.TabStop = false;
+            // 
+            // AI_PictureBox
+            // 
+            this.AI_PictureBox.BackColor = System.Drawing.Color.Transparent;
+            this.AI_PictureBox.Image = global::RockPaperScissors.Properties.Resources.question_mark;
+            this.AI_PictureBox.Location = new System.Drawing.Point(519, 74);
+            this.AI_PictureBox.Name = "AI_PictureBox";
+            this.AI_PictureBox.Size = new System.Drawing.Size(200, 200);
+            this.AI_PictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.AI_PictureBox.TabIndex = 0;
+            this.AI_PictureBox.TabStop = false;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Pb_Scissors);
+            this.Controls.Add(this.Pb_Paper);
+            this.Controls.Add(this.Pb_Rock);
+            this.Controls.Add(this.lab_choose_turn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lab_round);
+            this.Controls.Add(this.lab_time);
+            this.Controls.Add(this.lab_score);
+            this.Controls.Add(this.Player_PictureBox);
+            this.Controls.Add(this.AI_PictureBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
+            this.Text = "Rock-Paper-Scissors";
+            ((System.ComponentModel.ISupportInitialize)(this.Pb_Scissors)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pb_Paper)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Pb_Rock)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player_PictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AI_PictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox AI_PictureBox;
+        private System.Windows.Forms.PictureBox Player_PictureBox;
+        private System.Windows.Forms.Label lab_score;
+        private System.Windows.Forms.Label lab_time;
+        private System.Windows.Forms.Label lab_round;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lab_choose_turn;
+        private System.Windows.Forms.PictureBox Pb_Rock;
+        private System.Windows.Forms.PictureBox Pb_Paper;
+        private System.Windows.Forms.PictureBox Pb_Scissors;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ RockPaperScissors/RockPaperScissors/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace RockPaperScissors
+{
+    public partial class Main : Form
+    {
+        int rounds = 3;
+        bool gameover = false;
+        int Random_Number = 0;
+        int Global_Rounds;
+        Random random = new Random();
+        string AI_Turn, Player_Turn;
+        int AI_Wins, Player_Wins;
+        int RoundTimer = 4;
+        string[] AI_turn_list = { "rock", "paper", "scissors"};
+
+        public Main()
+        {
+            InitializeComponent();
+            timer1.Enabled = true;
+            Player_Turn = "none";
+            lab_time.Text = " 3 ";
+        }
+        private void Pb_Paper_Click(object sender, EventArgs e)
+        {
+            Player_PictureBox.Image = Properties.Resources.old_paper;
+            Player_Turn = "paper";
+        }
+
+        private void Pb_Scissors_Click(object sender, EventArgs e)
+        {
+            Player_PictureBox.Image = Properties.Resources.scissors;
+            Player_Turn = "scissors";
+        }
+
+        private void Pb_Rock_Click(object sender, EventArgs e)
+        {
+            Player_PictureBox.Image = Properties.Resources.stone;
+            Player_Turn = "rock";
+        }
+
+        public void NextRound()
+        {
+            if (gameover)
+            {
+                return;
+            }
+
+            lab_score.Text = $"{Player_Wins} - {AI_Wins}";
+
+            Player_Turn = "none";
+
+            timer1.Enabled = true;
+
+            Player_PictureBox.Image = Properties.Resources.question_mark;
+            AI_PictureBox.Image = Properties.Resources.question_mark;
+        }
+
+        public void CheckGame()
+        {
+            if (Player_Turn == "rock" && AI_Turn == "paper")
+            {
+
+                AI_Wins += 1;
+                Global_Rounds += 1;
+                rounds -= 1;
+
+                MessageBox.Show("CPU Wins, Paper Covers Rocks");
+
+            }
+            else if (Player_Turn == "scissors" && AI_Turn == "rock")
+            {
+                AI_Wins += 1;
+                Global_Rounds += 1;
+                rounds -= 1;
+
+                MessageBox.Show("CPU Wins, Rock Breaks Scissors");
+            }
+            else if (Player_Turn == "paper" && AI_Turn == "scissors")
+            {
+
+                AI_Wins += 1;
+                Global_Rounds += 1;
+                rounds -= 1;
+
+                MessageBox.Show("CPU Wins, Scissor cuts paper");
+
+            }
+            else if (Player_Turn == "rock" && AI_Turn == "scissors")
+            {
+
+                Player_Wins += 1;
+                Global_Rounds += 1;
+                rounds -= 1;
+
+                MessageBox.Show("Player Wins, Rock Breaks Scissors");
+
+            }
+            else if (Player_Turn == "paper" && AI_Turn == "rock")
+            {
+
+                Player_Wins += 1;
+                Global_Rounds += 1;
+                rounds -= 1;
+
+                MessageBox.Show("Player Wins, Paper Covers Rocks");
+
+            }
+            else if (Player_Turn == "scissors" && AI_Turn == "paper")
+            {
+                Player_Wins += 1;
+                Global_Rounds += 1;
+                rounds -= 1;
+
+                MessageBox.Show("Player Wins, Scissors cuts paper");
+
+            }
+            else if (Player_Turn == "none")
+            {
+                MessageBox.Show("Make your Choice");
+                Global_Rounds += 1;
+                rounds -= 1;
+            }
+            else
+            {
+                MessageBox.Show("Draw");
+                Global_Rounds += 1;
+                rounds -= 1;
+            }
+            NextRound();
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            RoundTimer -= 1;
+
+            lab_time.Text = RoundTimer.ToString();
+            lab_round.Text = $"Rounds: {rounds}";
+
+            if (RoundTimer < 1)
+            {
+                timer1.Enabled = false;
+                RoundTimer = 4;
+
+                Random_Number = random.Next(0, AI_turn_list.Length);
+                AI_Turn = AI_turn_list[Random_Number];
+
+                switch (AI_Turn)
+                {
+                    case "rock":
+                        AI_PictureBox.Image = Properties.Resources.stone;
+                        break;
+                    case "paper":
+                        AI_PictureBox.Image = Properties.Resources.old_paper;
+                        break;
+                    case "scissors":
+                        AI_PictureBox.Image = Properties.Resources.scissors;
+                        break;
+                }
+
+
+                if (rounds > 0)
+                {
+                    CheckGame();
+                }
+                else
+                {
+                    if (Player_Wins > AI_Wins)
+                    {
+                        MessageBox.Show("Player Wins This Game");
+                    }
+                    else
+                    {
+                        MessageBox.Show("AI Wins This Game");
+                    }
+                    gameover = true;
+                    if (Global_Rounds == 3)
+                    {
+                        RestartGame();
+                    }
+                }
+            }
+        }
+
+        private void RestartGame()
+        {
+            Player_Wins = 0;
+            AI_Wins = 0;
+            rounds = 3;
+            lab_score.Text = $"{Player_Wins} - {AI_Wins}";
+
+            Player_Turn = "none";
+            lab_time.Text = "3";
+
+            timer1.Enabled = true;
+
+            Player_PictureBox.Image = Properties.Resources.question_mark;
+            AI_PictureBox.Image = Properties.Resources.question_mark;
+
+            gameover = false;
+        }
+    }
+}
